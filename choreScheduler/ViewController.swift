4b210008254c5947df8be32bd6e7c6e71d8c2574
Billy Manners <billymanners@billys-imac.lan>
2020-07-11 13:07:38 +0100

    Initial Commit

---
+++ choreScheduler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  choreScheduler
+//
+//  Created by Billy Manners on 11/07/2020.
+//  Copyright © 2020 Billy Manners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
