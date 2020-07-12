0a524e6f302e91329edb255bd4b95132dbda3933
Billy Manners <billymanners@billys-imac.lan>
2020-07-12 11:32:14 +0100

    Done for now

---
+++ choreScheduler/Task.swift
@@ -0,0 +1,18 @@
+//
+//  task.swift
+//  choreScheduler
+//
+//  Created by Billy Manners on 12/07/2020.
+//  Copyright © 2020 Billy Manners. All rights reserved.
+//
+
+import Foundation
+
+class Task{
+    let taskName: String
+    let taskParticipants: [String]
+    let taskDate: Date()
+    let repeatingWeekly: Bool
+    init (name: String, participants: [String], date: Date(), repeatingWeekly: Bool)
+    
+}
